e87b036e8e59d0c35734c7dda2570c670a389a26
DESKTOP-AUSHM30\Thomas <thomasbenmezded@gmail.com>
2022-11-26 11:03:12 +0100

    end of project

--- src/app/border-card.directive.ts
+++ src/app/border-card.directive.ts
@@ -1,10 +1,21 @@
-import { Directive } from '@angular/core';
+// import { Directive } from '@angular/core';
 
-@Directive({
-  selector: '[appBorderCard]'
-})
-export class BorderCardDirective {
+// @Directive({
+//   selector: '[pkmnBorderCard]'
+// })
+// export class BorderCardDirective {
 
-  constructor() { }
+//   constructor(private el: ElementRef) {
+//     this.setHeight(180);
+//     this.setBorder(`#f5f5f5`);
+//    }
 
-}
+// setHeight(height: number){
+//   this.el.nativeElement.style.height =` $(height)px`;
+// }
+
+// setBorder(color: string){
+//   this.el.nativeElement.style.border = `solid 4px ${color} `;
+// }
+
+// }
