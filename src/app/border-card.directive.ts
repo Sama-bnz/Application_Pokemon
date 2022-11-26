ec0041a204c8f1d55b00b8e5ec96aa3427660904
DESKTOP-AUSHM30\Thomas <thomasbenmezded@gmail.com>
2022-11-26 11:29:37 +0100

    integrate a directive and learning about it

--- src/app/border-card.directive.ts
+++ src/app/border-card.directive.ts
@@ -1,21 +1,31 @@
-// import { Directive } from '@angular/core';
+import { Directive, ElementRef, HostListener, Input } from '@angular/core';
 
-// @Directive({
-//   selector: '[pkmnBorderCard]'
-// })
-// export class BorderCardDirective {
+@Directive({
+  selector: '[pkmnBorderCard]'
+})
+export class BorderCardDirective {
 
-//   constructor(private el: ElementRef) {
-//     this.setHeight(180);
-//     this.setBorder(`#f5f5f5`);
-//    }
+  constructor(private el: ElementRef) {
+    this.setBorder('#f5f5f5');
+    this.setHeight(180);
+   }
 
-// setHeight(height: number){
-//   this.el.nativeElement.style.height =` $(height)px`;
-// }
+  //  Changement de la couleur de bordure de carte quand la souris est sur une carte pokemon
+  @HostListener('mouseenter') onMouseEnter(){
+    this.setBorder('#009688');
+  }
+    //  Retour à la couleur initial quand la souris quitte la carte pokémon
+  @HostListener('mouseleave') onMouseLeave() {
+    this.setBorder('#f5f5f5');
+  }
 
-// setBorder(color: string){
-//   this.el.nativeElement.style.border = `solid 4px ${color} `;
-// }
 
-// }
+  setHeight(height: number) {
+    this.el.nativeElement.style.height = `${height}px`;
+  }
+
+  setBorder(color: string) {
+    this.el.nativeElement.style.border = `solid 4px ${color}`;
+  }
+
+}
