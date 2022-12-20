61b6d633d09fec764cefe9cb03a8727ea87682fb
DESKTOP-AUSHM30\Thomas <thomasbenmezded@gmail.com>
2022-12-20 09:23:53 +0100

    Add Pokémon to do a REAL Pokédex

--- src/app/mock-pokemon-list.ts
+++ src/app/mock-pokemon-list.ts
@@ -65,6 +65,51 @@ export const POKEMONS: Pokemon[] = [
         created: new Date()
     },
     {
+        id: 4,
+        name: "Carabaffe",
+        hp: 31,
+        cp: 8,
+        picture: "assets/images/carabaffe.png",
+        types: ["Eau"],
+        created: new Date()
+    },
+    {
+        id: 4,
+        name: "Tortank",
+        hp: 45,
+        cp: 12,
+        picture: "assets/images/tortank.png",
+        types: ["Eau"],
+        created: new Date()
+    },
+    {
+        id: 4,
+        name: "Chenipan",
+        hp: 15,
+        cp: 2,
+        picture: "assets/images/chenipan.png",
+        types: ["Insecte"],
+        created: new Date()
+    },
+    {
+        id: 4,
+        name: "Chrysacier",
+        hp: 20,
+        cp: 4,
+        picture: "assets/images/chrysacier.png",
+        types: ["Insecte"],
+        created: new Date()
+    },
+    {
+        id: 4,
+        name: "Papilusion",
+        hp: 30,
+        cp: 8,
+        picture: "assets/images/papilusion.png",
+        types: ["Insecte","Vol"],
+        created: new Date()
+    },
+    {
         id: 5,
         name: "Aspicot",
         hp: 16,
@@ -74,11 +119,47 @@ export const POKEMONS: Pokemon[] = [
         created: new Date()
     },
     {
+        id: 5,
+        name: "Coconfort",
+        hp: 20,
+        cp: 4,
+        picture: "assets/images/cocon.png",
+        types: ["Insecte", "Poison"],
+        created: new Date()
+    },
+    {
+        id: 5,
+        name: "Dardagnan",
+        hp: 25,
+        cp: 7,
+        picture: "assets/images/dardagnan.png",
+        types: ["Insecte", "Poison"],
+        created: new Date()
+    },
+    {
         id: 6,
         name: "Roucool",
-        hp: 30,
+        hp: 15,
         cp: 7,
         picture: "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/016.png",
+        types: ["Normal", "Vol"],
+        created: new Date()
+    },
+    {
+        id: 5,
+        name: "Roucoups",
+        hp: 25,
+        cp: 9,
+        picture: "assets/images/roucoups.png",
+        types: ["Normal", "Vol"],
+        created: new Date()
+    },
+    {
+        id: 5,
+        name: "Roucarnage",
+        hp: 35,
+        cp: 18,
+        picture: "assets/images/roucarnage.png",
         types: ["Normal", "Vol"],
         created: new Date()
     },
@@ -92,11 +173,38 @@ export const POKEMONS: Pokemon[] = [
         created: new Date()
     },
     {
+        id: 7,
+        name: "Rattatac",
+        hp: 25,
+        cp: 9,
+        picture: "assets/images/rattatac.png",
+        types: ["Normal"],
+        created: new Date()
+    },
+    {
         id: 8,
         name: "Piafabec",
         hp: 14,
         cp: 5,
         picture: "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/021.png",
+        types: ["Normal", "Vol"],
+        created: new Date()
+    },
+    {
+        id: 8,
+        name: "Piafabec",
+        hp: 14,
+        cp: 5,
+        picture: "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/021.png",
+        types: ["Normal", "Vol"],
+        created: new Date()
+    },
+    {
+        id: 8,
+        name: "Rapasdepic",
+        hp: 24,
+        cp: 15,
+        picture: "assets/images/rapasdepic.png",
         types: ["Normal", "Vol"],
         created: new Date()
     },
@@ -110,6 +218,15 @@ export const POKEMONS: Pokemon[] = [
         created: new Date()
     },
     {
+        id: 9,
+        name: "Arbok",
+        hp: 26,
+        cp: 10,
+        picture: "assets/images/arbok.png",
+        types: ["Poison"],
+        created: new Date()
+    },
+    {
         id: 10,
         name: "Pikachu",
         hp: 21,
@@ -119,12 +236,48 @@ export const POKEMONS: Pokemon[] = [
         created: new Date()
     },
     {
+        id: 10,
+        name: "Raichu",
+        hp: 31,
+        cp: 17,
+        picture: "assets/images/raichu.png",
+        types: ["Electrik"],
+        created: new Date()
+    },
+    {
         id: 11,
         name: "Sabelette",
         hp: 19,
         cp: 3,
         picture: "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/027.png",
-        types: ["Normal"],
+        types: ["Sol"],
+        created: new Date()
+    },
+    {
+        id: 11,
+        name: "Sablaireau",
+        hp: 29,
+        cp: 13,
+        picture: "assets/images/sablaireau.png",
+        types: ["Sol"],
+        created: new Date()
+    },
+    {
+        id: 9,
+        name: "Nidoran♀",
+        hp: 26,
+        cp: 10,
+        picture: "assets/images/nidoran_female.png",
+        types: ["Poison"],
+        created: new Date()
+    },
+    {
+        id: 9,
+        name: "Nidorina",
+        hp: 28,
+        cp: 12,
+        picture: "assets/images/nidorina.png",
+        types: ["Poison"],
         created: new Date()
     },
     {
